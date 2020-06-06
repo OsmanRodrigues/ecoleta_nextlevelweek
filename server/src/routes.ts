d620babcc6dfaac23dcf1e2080176f457f945da7
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-06-06 16:13:09 -0300

    Pequenos ajustes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import express, { response, request } from 'express';
+import {celebrate, Joi} from 'celebrate';
 
 import multer from 'multer';
 import multerConfig from './config/multer';
@@ -18,7 +19,25 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index );
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', uplodad.single('image'), pointsController.create);
+routes.post(
+    '/points', 
+    uplodad.single('image'),
+    celebrate({
+        body:Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number(). required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    },{
+        abortEarly: false,
+    }),
+    pointsController.create
+);
 
 // Index, show, create, delete (padrão dos constructors controllors)
 
